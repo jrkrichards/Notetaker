fbd6efec37c4c6324cc09c181c14793857abb45c
Jayrich <jrkrichards@gmail.com>
2020-12-11 07:46:53 -0800

    server, router and middleware is set up. Need to fix and connect to the db.json

--- server/routes/apiRoutes.js
+++ server/routes/apiRoutes.js
@@ -1,12 +1,16 @@
 const router = require("express").Router();
 const fs = require("fs");
+const dbInfo = fs.readFile("../db/db.json", "utf8", (err, data) => {
+    if (err) throw err;
+    console.log(JSON.parse(data));
+  });
 
 // Allowing the client to access specific data
 router.get("/", (req, res) => {
     console.log("we have been hit")
-    fs.readFile("../../db/db.json", "utf8", (err,data) => {
+    fs.readFile("../db/db.json", "utf8", (err,data) => {
         if(err) throw err;
-        res.json(JSON.parse(data));
+        console.log(res.json(JSON.parse(data)));
     });
 });
 
