99d78bcd106c4c2af83c3bdff79ae96b03d2525e
Jayrich <jrkrichards@gmail.com>
2020-12-14 07:03:47 -0800

    UPDATE apiRoutes.js: created get request to access the current notes

--- server/routes/apiRoutes.js
+++ server/routes/apiRoutes.js
@@ -4,17 +4,58 @@ const fs = require("fs");
 // Allowing the client to access specific data
 const dbInfo = fs.readFile("../db/db.json", "utf8", (err, data) => {
     if (err) throw err;
-    console.log("api side hit")
     console.log(JSON.parse(data));
   });
 
-// // Allowing the client to access specific data
-// router.get("/", (req, res) => {
-//     console.log("we have been hit")
-//     fs.readFile("../db/db.json", "utf8", (err,data) => {
+// Allowing the client to access specific data
+router.get("/", (req, res) => {
+    console.log("we have been hit")
+    fs.readFile("../db/db.json", "utf8", (err,data) => {
+        if(err) throw err;
+        res.json(JSON.parse(data));
+    });
+});
+
+// To receive data from the url.
+router.get("/notes", (req,res) => {
+    fs.readFile("../db/db.json", "utf8", (err,data) => {
+        if(err) throw err;
+        
+        const allNotes = JSON.parse(data)
+
+        for (let i = 0; i < allNotes.length; i++) {
+            return res.json(allNotes[i]);
+        };
+        
+        return res.json({
+            msg:"There are no saved notes."
+        });
+    });
+});    
+
+// post new notes
+// router.post("/characters/new", (req, res) => {
+//     console.log(req.body);
+//     fs.readFile("./data.json", "utf8", (err, data) => {
 //         if(err) throw err;
-//         console.log(res.json(JSON.parse(data)));
+//         const allCharacters = JSON.parse(data);
+//         console.log(req.body)
+//         allCharacters.push({
+//             routeName: req.body.routeName,
+//             name: req.body.name,
+//             role: req.body.role,
+//             age: parseInt(req.body.age),
+//             forcePoints: parseInt(req.body.forcePoints)
+//         })
+//         console.log(allCharacters)
+
+//         fs.writeFile("./data.json", JSON.stringify(allCharacters), (err) => {
+//             if(err) throw err;
+//             res.json({
+//                 msg: "success fully added"
+//             })
+//         })
 //     });
-// });
+// })
 
 module.exports = router
