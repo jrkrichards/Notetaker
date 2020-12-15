d9083871a6ea841737bd62e44c0637682723593c
Jayrich <jrkrichards@gmail.com>
2020-12-15 06:26:33 -0800

    updated routes to fix post error but now applications is working as expected.

--- server/routes/apiRoutes.js
+++ server/routes/apiRoutes.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const fs = require("fs");
 
-// Allowing the client to access specific data
+// Viewing current notes in db
 const dbInfo = fs.readFile("../db/db.json", "utf8", (err, data) => {
     if (err) throw err;
     console.log(JSON.parse(data));
